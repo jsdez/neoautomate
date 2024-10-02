7a70860e758694963555c1474a28fd62aa9685cc
Jake Dennison <jake.dennison@hotmail.co.uk>
2024-10-02 16:15:24 +0100

    .

---
+++ neo-automate-site/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="text-center mt-5">Hello, Bootstrap!</h1>
+      <button className="btn btn-primary">Click Me</button>
+    </div>
+  );
+}
+
+export default App;
