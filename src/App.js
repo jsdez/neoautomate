ffdd87ccb8f23af97a4808d728749981670f0627
Jake Dennison <jake.dennison@hotmail.co.uk>
2024-10-02 16:34:48 +0100

    .

--- src/App.js
+++ src/App.js
@@ -3,9 +3,44 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="container">
-      <h1 className="text-center mt-5">Hello, Bootstrap!</h1>
-      <button className="btn btn-primary">Click Me</button>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <a className="navbar-brand" href="#">Neo Automate</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="container mt-5">
+        <div className="jumbotron">
+          <h1 className="display-4">Welcome to Neo Automate</h1>
+          <p className="lead">We build automation solutions for modern businesses.</p>
+          <hr className="my-4" />
+          <p>Get started with our solutions and take your business to the next level.</p>
+          <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </div>
+      </div>
+
+      <footer className="bg-light py-3 mt-5">
+        <div className="container">
+          <p className="text-center">© 2024 Neo Automate. All rights reserved.</p>
+        </div>
+      </footer>
     </div>
   );
 }
