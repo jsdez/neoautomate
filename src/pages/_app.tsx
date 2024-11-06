18b66abad1e5c17f3cb3da31952ee3055c67b279
Jake Dennison <jake.dennison@hotmail.co.uk>
2024-11-06 15:51:48 +0000

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { AppProps } from 'next/app';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import "@/styles/styles.scss";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
