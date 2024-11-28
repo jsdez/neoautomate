8a9943dca98e52b38727dd268971190e0c148ec0
Jake Dennison <jake.dennison@hotmail.co.uk>
2024-11-28 13:17:39 +0000

    Updates

--- _next/static/ZvQo3P_J84Wu3Vpvpdd-f/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/207-9899473331c80377.js","static/chunks/pages/index-b59d0359f17cd874.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
